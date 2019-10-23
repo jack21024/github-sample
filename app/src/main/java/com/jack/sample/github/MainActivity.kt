ad151d9d024b9f1da5b8606cd720b0c62f6aba4d
toosakarin <jack21024@gmail.com>
2019-10-23 17:53:42 +0800

    Create empty main page.

--- app/src/main/java/com/jack/sample/github/MainActivity.kt
+++ app/src/main/java/com/jack/sample/github/MainActivity.kt
@@ -2,11 +2,35 @@ package com.jack.sample.github
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.jack.sample.github.databinding.ActivityMainBinding
+import androidx.lifecycle.ViewModelProviders
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var _binding: ActivityMainBinding
+    private lateinit var _viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initBinding()
+        initAllList()
     }
+
+    private fun initBinding() {
+        _viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        _binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        _binding.viewModel = _viewModel
+        _binding.lifecycleOwner = this
+    }
+
+    private fun initAllList() {
+        _binding.mainListUser.let {
+            it.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        }
+        _binding.mainListRepo.let {
+            it.layoutManager = LinearLayoutManager(this)
+        }
+    }
+
 }
---
+++ app/src/main/java/com/jack/sample/github/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.jack.sample.github
+
+import androidx.databinding.ObservableBoolean
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    val loading: ObservableBoolean = ObservableBoolean(false)
+
+    
+}
