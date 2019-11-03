13c1eadf5b5271387bf18cf3b887c16253ed03d5
toosakarin <jack21024@gmail.com>
2019-11-03 19:47:32 +0800

    [Repo] Apply newly layout.

--- app/src/main/java/com/jack/sample/github/model/UserRepo.kt
+++ app/src/main/java/com/jack/sample/github/model/UserRepo.kt
@@ -3,7 +3,8 @@ package com.jack.sample.github.model
 import com.google.gson.annotations.SerializedName
 
 data class UserRepo(
-    @SerializedName("name") val name: String,
+    @SerializedName("owner") val owner: User,
+    @SerializedName("full_name") val full_name: String,
     @SerializedName("language") val language: String,
     @SerializedName("description") val description: String,
     @SerializedName("updated_at") val updated_at: String,
--- app/src/main/java/com/jack/sample/github/view/githubcard/RepositoryCard.kt
+++ app/src/main/java/com/jack/sample/github/view/githubcard/RepositoryCard.kt
@@ -1,10 +1,15 @@
 package com.jack.sample.github.view.githubcard
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.jack.sample.github.R
 import com.jack.sample.github.model.UserRepo
 import kotlinx.android.synthetic.main.list_item_user_repo.view.*
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.*
 
 class RepositoryCard(private val data: UserRepo?) : BaseCard() {
 
@@ -19,13 +24,29 @@ class RepositoryCard(private val data: UserRepo?) : BaseCard() {
 
     override fun bindViewHolder(holder: BaseViewHolder) {
         if (holder is ViewHolder) {
-            holder.desc.text = data!!.description
-            holder.update_at.text = data!!.updated_at
+            data?.apply {
+                holder.full_name.text = this.full_name
+                holder.desc.text = this.description
+                holder.update_at.text = formatDate(this.updated_at)
+                holder.starCount.text = this.stargazers_count
+                Glide.with(holder.avatar)
+                    .load(this.owner.avatar_url)
+                    .placeholder(R.drawable.placeholder_circle_gray_24)
+                    .circleCrop()
+                    .into(holder.avatar)
+            }
         }
     }
 
+    private fun formatDate(dateStr: String) : String {
+        return dateStr.split("T")[0]
+    }
+
     class ViewHolder(itemView: View) : BaseViewHolder(itemView) {
+        val full_name: TextView = itemView.item_userrepo_tv_fullname
         val desc: TextView = itemView.item_userrepo_tv_desc
         val update_at: TextView = itemView.item_userrepo_tv_update_at
+        val starCount: TextView = itemView.item_userrepo_tv_star_count
+        val avatar: ImageView = itemView.item_userrepo_iv_avatar
     }
 }
