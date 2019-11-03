673b04ebb6dbdff05a6b672fa9b14c556cb92466
toosakarin <jack21024@gmail.com>
2019-11-03 17:28:11 +0800

    [Repo] Update model.

--- app/src/main/java/com/jack/sample/github/model/UserRepo.kt
+++ app/src/main/java/com/jack/sample/github/model/UserRepo.kt
@@ -3,6 +3,9 @@ package com.jack.sample.github.model
 import com.google.gson.annotations.SerializedName
 
 data class UserRepo(
+    @SerializedName("name") val name: String,
+    @SerializedName("language") val language: String,
     @SerializedName("description") val description: String,
-    @SerializedName("updated_at") val updated_at: String
+    @SerializedName("updated_at") val updated_at: String,
+    @SerializedName("stargazers_count") val stargazers_count: String
 )
